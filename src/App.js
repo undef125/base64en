28c5aaaf6d8c3cf295eb4b1496705c5e5390d273
Undef125 <076bct038@ioepc.edu.np>
2022-04-13 17:03:56 +0545

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Converter from './converter.js'
+import {Helmet} from "react-helmet";
+import favicon from './favicon.ico';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Base 64</title>
+        <link rel="canonical" type="image/png" href={favicon} sizes="16x16" />
+      </Helmet>
       <Converter/>
     </div>
   );
